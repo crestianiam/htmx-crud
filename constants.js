fa67186b34e336046b9aade2c638725737897f0b
crestianiam <andrian.crestian95@gmail.com>
2024-07-07 23:26:53 +0200

    create and cancel create

--- constants.js
+++ constants.js
@@ -1,2 +1,4 @@
 export const VIEW_TODO_ITEM = "todo-item.html";
 export const VIEW_TODO_EMPTY_LIST = "todo-empty-list.html";
+export const VIEW_FORM_NEW_ITEM = "form-new.html";
+export const VIEW_ITEM_SECTION = "item-section.html";
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ import { sleep } from "../utils.js";
 import { uuid } from "uuidv4";
 import fs from "fs";
 import dotenv from "dotenv";
-import { VIEW_TODO_ITEM, VIEW_TODO_EMPTY_LIST } from "../constants.js";
+import { VIEW_TODO_ITEM, VIEW_TODO_EMPTY_LIST, VIEW_FORM_NEW_ITEM, VIEW_ITEM_SECTION } from "../constants.js";
 import { readTodos, saveTodos } from "../persistance/readAndWrite.js";
 
 dotenv.config();
@@ -19,9 +19,9 @@ router.get("/todos", async (req, res) => {
   const todoItemsHTML = todos.map(todo => {
     const todoItemHTML = fs.readFileSync(path.join(viewsFolderPath, VIEW_TODO_ITEM), 'utf8');
     return todoItemHTML
+      .replace(/\{\{ todo\.id \}\}/g, todo.id)
       .replace(/\{\{ todo\.name \}\}/g, todo.name)
-      .replace(/\{\{ todo\.description \}\}/g, todo.description)
-      .replace(/\{\{ todo\.id \}\}/g, todo.id);
+      .replace(/\{\{ todo\.description \}\}/g, todo.description);
   }).join('');
   const todoEmptyListHTML = fs.readFileSync(path.join(viewsFolderPath, VIEW_TODO_EMPTY_LIST), 'utf8');
 
@@ -33,43 +33,15 @@ router.get("/todos", async (req, res) => {
     todoEmptyListHTML);
 })
 
-router.get('/todos/:id', (req, res) => {
-  const { id } = req.params;
-  const todos = readTodos();
-  const todo = todos.find((todo) => todo.id === Number(id));
-
-  res.send(`
-    <h2>${todo.name}</h2>
-    <p><strong>Name:</strong> ${todo.name}</p>
-    <p><strong>Description:</strong> ${todo.description}</p>
-  `);
+router.get('/todos/new', (req, res) => {
+  const formHTML = fs.readFileSync(path.join(viewsFolderPath, VIEW_FORM_NEW_ITEM), 'utf8');
+  res.send(formHTML);
 });
 
-router.delete('/todos/:id', (req, res) => {
-  const { id } = req.params;
-  const todos = readTodos();
-  console.log(todos)
-  const indexOfElement = todos.findIndex((current) => current.id === id);
-  let newTodos = [...todos];
-  if (indexOfElement !== 1) {
-    newTodos.splice(indexOfElement, 1);
-  }
-  saveTodos(newTodos);
-
-  const todoItemsHTML = newTodos.map(todo => {
-    const todoItemHTML = fs.readFileSync(path.join(viewsFolderPath, VIEW_TODO_ITEM), 'utf8');
-    return todoItemHTML
-      .replace('{{ todo.name }}', todo.name)
-      .replace('{{ todo.description }}', todo.description)
-      .replace('{{ todo.id }}', todo.id);
-  }).join('');
-
-  res.send(`
-     <ul>
-       ${todoItemsHTML}
-     </ul>
-   `);
-})
+router.get('/todos/cancel-action', (req, res) => {
+  const formHTML = fs.readFileSync(path.join(viewsFolderPath, VIEW_ITEM_SECTION), 'utf8');
+  res.send(formHTML);
+});
 
 router.post("/todos", (req, res) => {
   const newToDo = req.body;
@@ -81,9 +53,9 @@ router.post("/todos", (req, res) => {
   const todoItemsHTML = todos.map(todo => {
     const todoItemHTML = fs.readFileSync(path.join(viewsFolderPath, VIEW_TODO_ITEM), 'utf8');
     return todoItemHTML
-      .replace('{{ todo.name }}', todo.name)
-      .replace('{{ todo.description }}', todo.description)
-      .replace('{{ todo.id }}', todo.id);
+      .replace(/\{\{ todo\.id \}\}/g, todo.id)
+      .replace(/\{\{ todo\.name \}\}/g, todo.name)
+      .replace(/\{\{ todo\.description \}\}/g, todo.description);
   }).join('');
 
   res.send(`
@@ -91,6 +63,36 @@ router.post("/todos", (req, res) => {
       ${todoItemsHTML}
     </ul>
   `);
+})
+
+
+router.delete('/todos/:id', (req, res) => {
+  const { id } = req.params;
+  const todos = readTodos();
+  const indexOfElement = todos.findIndex((current) => current.id === id);
+  let newTodos = [...todos];
+  if (indexOfElement > -1) {
+    newTodos.splice(indexOfElement, 1);
+  }
+  saveTodos(newTodos);
+
+  const todoItemsHTML = newTodos.map(todo => {
+    const todoItemHTML = fs.readFileSync(path.join(viewsFolderPath, VIEW_TODO_ITEM), 'utf8');
+    return todoItemHTML
+      .replace(/\{\{ todo\.id \}\}/g, todo.id)
+      .replace(/\{\{ todo\.name \}\}/g, todo.name)
+      .replace(/\{\{ todo\.description \}\}/g, todo.description);
+  }).join('');
+
+  res.send(`
+     <ul>
+       ${todoItemsHTML}
+     </ul>
+   `);
+})
+
+router.put('/todos/:id', (req, res) => {
+
 })
 
 
